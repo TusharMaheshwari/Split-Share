ebd5a2da44ded4009dd7bf4d31628a2ff674156e
Shivansh Mishra <shivansh.mishra0820@gmail.com>
2025-01-10 15:56:00 +0530

    Test Commit.

--- app/src/main/java/com/example/splitshare/MainActivity.kt
+++ app/src/main/java/com/example/splitshare/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             SplitShareTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "Shivansh Android",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
