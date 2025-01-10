ec8a89235d7b58c1dece4e30734953ef43cd669e
TusharMaheshwari <tusharmaheshwari100@gmail.com>
2025-01-10 17:13:54 +0530

    Added Theme
    Signed-off-by: TusharMaheshwari <tusharmaheshwari100@gmail.com>

--- app/src/main/java/com/example/splitshare/ui/theme/Theme.kt
+++ app/src/main/java/com/example/splitshare/ui/theme/Theme.kt
@@ -12,26 +12,69 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = theme_dark_primary,
+    onPrimary = theme_dark_onPrimary,
+    primaryContainer = theme_dark_primaryContainer,
+    onPrimaryContainer = theme_dark_onPrimaryContainer,
+    secondary = theme_dark_secondary,
+    onSecondary = theme_dark_onSecondary,
+    secondaryContainer = theme_dark_secondaryContainer,
+    onSecondaryContainer = theme_dark_onSecondaryContainer,
+    tertiary = theme_dark_tertiary,
+    onTertiary = theme_dark_onTertiary,
+    tertiaryContainer = theme_dark_tertiaryContainer,
+    onTertiaryContainer = theme_dark_onTertiaryContainer,
+    error = theme_dark_error,
+    errorContainer = theme_dark_errorContainer,
+    onError = theme_dark_onError,
+    onErrorContainer = theme_dark_onErrorContainer,
+    background = theme_dark_background,
+    onBackground = theme_dark_onBackground,
+    surface = theme_dark_surface,
+    onSurface = theme_dark_onSurface,
+    surfaceVariant = theme_dark_surfaceVariant,
+    onSurfaceVariant = theme_dark_onSurfaceVariant,
+    outline = theme_dark_outline,
+    inverseOnSurface = theme_dark_inverseOnSurface,
+    inverseSurface = theme_dark_inverseSurface,
+    inversePrimary = theme_dark_inversePrimary,
+    surfaceTint = theme_dark_surfaceTint,
+    outlineVariant = theme_dark_outlineVariant,
+    scrim = theme_dark_scrim,
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
+    primary = theme_light_primary,
+    onPrimary = theme_light_onPrimary,
+    primaryContainer = theme_light_primaryContainer,
+    onPrimaryContainer = theme_light_onPrimaryContainer,
+    secondary = theme_light_secondary,
+    onSecondary = theme_light_onSecondary,
+    secondaryContainer = theme_light_secondaryContainer,
+    onSecondaryContainer = theme_light_onSecondaryContainer,
+    tertiary = theme_light_tertiary,
+    onTertiary = theme_light_onTertiary,
+    tertiaryContainer = theme_light_tertiaryContainer,
+    onTertiaryContainer = theme_light_onTertiaryContainer,
+    error = theme_light_error,
+    errorContainer = theme_light_errorContainer,
+    onError = theme_light_onError,
+    onErrorContainer = theme_light_onErrorContainer,
+    background = theme_light_background,
+    onBackground = theme_light_onBackground,
+    surface = theme_light_surface,
+    onSurface = theme_light_onSurface,
+    surfaceVariant = theme_light_surfaceVariant,
+    onSurfaceVariant = theme_light_onSurfaceVariant,
+    outline = theme_light_outline,
+    inverseOnSurface = theme_light_inverseOnSurface,
+    inverseSurface = theme_light_inverseSurface,
+    inversePrimary = theme_light_inversePrimary,
+    surfaceTint = theme_light_surfaceTint,
+    outlineVariant = theme_light_outlineVariant,
+    scrim = theme_light_scrim,)
 
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
-)
+
 
 @Composable
 fun SplitShareTheme(
